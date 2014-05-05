957632a7e02e6735b6f33c1e09ddd36be8c55ded
Gard Sandholt <gard.sandholt@gmail.com>
2014-05-05 12:02:29 +0200

    Added comments to OperationQueue.java

--- library/src/main/java/com/javanetworking/operationqueue/OperationQueue.java
+++ library/src/main/java/com/javanetworking/operationqueue/OperationQueue.java
@@ -25,31 +25,87 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import com.javanetworking.operationqueue.Operation.OperationState;
 
-
+/**
+ The {@link OperationsQueue} class handles the execution of {@link Operation} instances
+ through a queue and executes them one by one in a separate thread.
+ */
 public class OperationQueue {
 
+	/**
+	 A {@link BlockingQueue} instance which acts at the main queue that holds added operations.
+	 */
 	private final BlockingQueue<Operation> mainQueue;
+
+	/**
+	 A {@link Thread} instance that takes and executes operations from the mainQueue.
+	 */
 	private Thread queueThread;
+
+	/**
+	 A boolean value inticating the running status of the queueThread. When set to false the queueThread
+	 exits when current operation finnishes.
+	 */
 	private boolean runningStatus = false;
 	
+  
+	//----------------------------------------
+	// @name Constructor and Instance creation
+	//----------------------------------------
+
+	/**
+	 Instantiate a new {@link OperationQueue} instance
+	 */
 	public OperationQueue() {
 		this.mainQueue = new LinkedBlockingQueue<Operation>();
 	}
 	
+	
+	//-----------------------------------
+	// @name Attribute and status methods
+	//-----------------------------------
+
+	/**
+	 Set the running status of current execution thread.
+	 
+	 @param status A boolean value indication if the running status should be true or false
+	 			   if false the current execution thread will exit after the next operations
+	 			   finishes. 
+	 */
 	public synchronized void setRunningStatus(boolean status) {
 		this.runningStatus = status;
 	}
 	
+	/**
+	 Return the running status boolean value.
+	 
+	 @return A boolean value indicating the running status of the current execution thread.
+	 		 If false the execution thread has been stopped, and will be started if another operation
+	 		 is added to the current instance of the {@link OperationQueue}.
+	 */
 	public synchronized boolean getRunningStatus() {
 		return this.runningStatus;
 	}
 	
+	/**
+	 Cancel all waiting operations and clear operations queue. This method sets the running
+	 status to false, interrupts the current execution thread which throws an {@link InterruptedException}
+	 and exits before finally clearing the main queue for waiting operations.
+	 */
 	public void cancelAllOperations() {
 		setRunningStatus(false);
 		this.queueThread.interrupt();
 		this.mainQueue.clear();
 	}
 	
+	/**
+	 Add {@link Operation} instance to the {@link OperationQueue} instance main queue. The {@linkplain}
+	 operations if offered to the main queue but can be rejected and not added to the queue. When 
+	 rejected the {@link Operation} has the state of {@link OperationState.Rejected}. If the {@link Operation}
+	 is added to the queue the state is set to {@link OperationState.InQueue}.
+	 
+	 It the {@link OperationQueue}s running status is false. The execution thread is started and the running
+	 status is updated to true.
+	 */
 	public void addOperation(Operation operation) {
 		if (getRunningStatus() != true) {
 			start();
@@ -62,16 +118,40 @@ public class OperationQueue {
 		}
 	}
 	
-	public boolean isEmpty() {
-		return mainQueue.isEmpty();
-	}
-	
+	/**
+	 Adds a {@link List} of instantiated {@link Operation} objects. The methods loops over the list
+	 and calls the {@code addOperation(Operation operation)} method.
+	 
+	 @param operations An instantiated {@link List} of instantiated {@link Operation} objects which is
+	 		added to the {@link OperationQueue}s main queue.
+	 */
 	public void addOperations(List<Operation> operations) {
 		for (Operation operation : operations) {
 			this.addOperation(operation);
 		}
 	}
 	
+	/**
+	 Check if the {@link OperationQueue}s main queue is empty.
+	 
+	 @return A boolean value indicating if the {@link OperationQueue} is empty. If true the {@link OperationQueue}
+	 		 has no waiting operations to execute.
+	 */
+	public boolean isEmpty() {
+		return mainQueue.isEmpty();
+	}
+	
+
+	//----------------------------------------------
+	// @name Execution thread and operation handling
+	//----------------------------------------------
+
+	/**
+	 Private method to instantiate and start the queueThread which takes {@link Operation} objects
+	 from the main queue when they are ready. The {@link BlockingQueue}s {@code take()} method is
+	 used since it retrieves and removes the head of main queue, waiting if necessary until an element 
+	 becomes available.
+	 */
 	private void start() {
 		queueThread = new Thread(new Runnable() {
 			@Override
