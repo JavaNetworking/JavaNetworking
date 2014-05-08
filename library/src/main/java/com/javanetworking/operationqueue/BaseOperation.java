ee9dacdc0fad9e162b2d324d5a0fb0c36cd593a1
Gard Sandholt <gard.sandholt@gmail.com>
2014-05-08 15:49:19 +0200

    Added comments

--- library/src/main/java/com/javanetworking/operationqueue/BaseOperation.java
+++ library/src/main/java/com/javanetworking/operationqueue/BaseOperation.java
@@ -20,29 +20,55 @@
 
 package com.javanetworking.operationqueue;
 
+/**
+ The base operation which can be sub-classed. All the basic operations 
+ attributes such as current running state, should be set in this class.
+ */
 public class BaseOperation implements Operation {
 
+	/**
+	 A {@link OperationState} value indicating the operations
+	 current operation state.
+	 */
 	private OperationState state = null;
 	
+	/**
+	 Basic constructor, sets the {@link OperationState} to {@link OperationState.Created}.
+	 */
 	public BaseOperation() {
 		setState(OperationState.Created);
 	}
 	
-	protected String getName() {
-		return BaseOperation.class.getName();
-	}
-	
+	/**
+	 Sets this operations state.
+	 
+	 @param state A {@link OperationState} value indicating the operation state to set.
+	 */
 	public synchronized void setState(OperationState state) {
 		this.state = state;
 	}
 	
+	/**
+	 Gets this operations {@link OperationState}.
+	 
+	 @return A {@link OperationState} value indicating the current operation state.
+	 */
 	public synchronized OperationState getState() {
 		return state;
 	}
 
+	/**
+	 The execution methods where executing code should be placed.
+	 This method is called in a separate thread on the {@link OperationQueue} queue which 
+	 holds this {@link Operation}.
+	 */
 	public synchronized void execute() {
 		setState(OperationState.Running);
 	}
+	
+	/**
+	 The completion methods which is called after the {@code execute()} method is called.
+ 	 */
 	public synchronized void complete() {
 		setState(OperationState.Finished);
 	}
