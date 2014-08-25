831d847f771ef8885fd98ab1d5e62b751b5db29e
Gard Sandholt <gard.sandholt@gmail.com>
2014-08-25 15:16:49 +0200

    Added GET and 404 test for HTTPURLRequestOperation

--- library/src/test/java/com/javanetworking/HTTPURLRequestOperationTest.java
+++ library/src/test/java/com/javanetworking/HTTPURLRequestOperationTest.java
@@ -2,6 +2,9 @@
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
 
 import com.javanetworking.HTTPURLRequestOperation.HTTPCompletion;
@@ -12,39 +15,79 @@ public class HTTPURLRequestOperationTest {
 	
 	@Test
 	public void testHTTPURLRequestOperationSuccess() {
+		final CountDownLatch signal = new CountDownLatch(1);
+
+		final StringBuilder errorSB = new StringBuilder();
+		final StringBuilder successSB = new StringBuilder();
 		
+		// Request and operation
 		URLRequest request = URLRequest.requestWithURLString(BASE_URL + "get");
 		
-		HTTPURLRequestOperation operation = HTTPURLRequestOperation.operationWithURLRequest(request, new HTTPCompletion() {
+		HTTPURLRequestOperation.operationWithURLRequest(request, new HTTPCompletion() {
 			@Override
 			public void failure(URLRequest request, Throwable t) {
-				assertNull(t);
+				errorSB.append(t.toString());
+
+				signal.countDown();
 			}
 			@Override
 			public void success(URLRequest request, Object response) {
-				assertNotNull(response);
+				successSB.append(response);
+
+				signal.countDown();
 			}
-		});
-		
-		operation.start();
+		}).start();
+
+		// Wait for signal count down
+		try {
+            signal.await(30, TimeUnit.SECONDS); // wait for callback
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+		// Test values
+		assertEquals("", errorSB.toString());
+
+		assertTrue(errorSB.toString().isEmpty());
+		assertFalse(successSB.toString().isEmpty());
 	}
 	
 	@Test
 	public void testHTTPURLRequestOperationFailure() {
-		
+		final CountDownLatch signal = new CountDownLatch(1);
+
+		final StringBuilder errorSB = new StringBuilder();
+		final StringBuilder successSB = new StringBuilder();
+
+		// Request and operation
 		URLRequest request = URLRequest.requestWithURLString(BASE_URL + "status/404");
-		
-		HTTPURLRequestOperation operation = HTTPURLRequestOperation.operationWithURLRequest(request, new HTTPCompletion() {
+
+		HTTPURLRequestOperation.operationWithURLRequest(request, new HTTPCompletion() {
 			@Override
 			public void failure(URLRequest request, Throwable t) {
-				assertNotNull(t);
+				errorSB.append(t.toString());
+
+				signal.countDown();
 			}
 			@Override
 			public void success(URLRequest request, Object response) {
-				assertNull(response);
+				successSB.append(response);
+
+				signal.countDown();
 			}
-		});
-		
-		operation.start();
+		}).start();
+
+		// Wait for signal count down
+		try {
+	        signal.await(30, TimeUnit.SECONDS); // wait for callback
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+
+		// Test values
+		assertEquals("", successSB.toString());
+
+		assertFalse(errorSB.toString().isEmpty());
+		assertTrue(successSB.toString().isEmpty());
 	}
 }
