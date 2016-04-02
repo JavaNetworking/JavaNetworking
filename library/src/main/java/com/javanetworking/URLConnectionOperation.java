e301ce95c6f2f1eab2943b25a951f70a232f261b
Gard Sandholt <gard.sandholt@gmail.com>
2016-04-02 13:29:55 +0200

    Fixed tab indentation

--- library/src/main/java/com/javanetworking/URLConnectionOperation.java
+++ library/src/main/java/com/javanetworking/URLConnectionOperation.java
@@ -14,121 +14,121 @@ import com.operationqueue.OperationQueue;
  */
 public class URLConnectionOperation extends BaseOperation {
 
-	/**
-	 {@link URLCompletion} is {@link URLRequestRequest}s completion interface which indicates the
-	 {@link URLRequest} failed or succeeded.
-	 */
-	public interface URLCompletion {
-		void failure(URLRequest urlRequest, Throwable t);
-		void success(URLRequest urlRequest, byte[] responseData);
-	}
+    /**
+     {@link URLCompletion} is {@link URLRequestRequest}s completion interface which indicates the
+     {@link URLRequest} failed or succeeded.
+     */
+    public interface URLCompletion {
+        void failure(URLRequest urlRequest, Throwable t);
+        void success(URLRequest urlRequest, byte[] responseData);
+    }
 
-	/**
-	 A static constructor method that creates and returns a {@link URLRequestRequest} instance.
-	 */
-	public static URLConnectionOperation operationWithURLRequest(URLRequest urlRequest, URLCompletion completion) {
-		return new URLConnectionOperation(urlRequest, completion);
-	}
+    /**
+     A static constructor method that creates and returns a {@link URLRequestRequest} instance.
+     */
+    public static URLConnectionOperation operationWithURLRequest(URLRequest urlRequest, URLCompletion completion) {
+        return new URLConnectionOperation(urlRequest, completion);
+    }
 
-	/**
-	 The URL request.
-	 */
-	private URLRequest urlRequest;
+    /**
+     The URL request.
+     */
+    private URLRequest urlRequest;
 
-	/**
-	 The instance completion interface.
-	 */
-	private URLCompletion completion;
+    /**
+     The instance completion interface.
+     */
+    private URLCompletion completion;
 
-	/**
-	 Response accumulation buffer, a byte array output stream.
-	 */
-	private ByteArrayOutputStream accumulationBuffer;
+    /**
+     Response accumulation buffer, a byte array output stream.
+     */
+    private ByteArrayOutputStream accumulationBuffer;
 
-	/**
-	 Instantiates this class and sets the {@link URLRequest} to use, and the {@link URLCompletion} interface.
+    /**
+     Instantiates this class and sets the {@link URLRequest} to use, and the {@link URLCompletion} interface.
 
-	 This is the preferred constructor.
+     This is the preferred constructor.
 
-	 @param urlRequest An open {@link URLRequest} to be used for network access.
+     @param urlRequest An open {@link URLRequest} to be used for network access.
      @param completion A {@link URLCompletion} instance that handles the completion interface methods.
-	 */
-	public URLConnectionOperation(URLRequest urlRequest, URLCompletion completion) {
-		super();
+     */
+    public URLConnectionOperation(URLRequest urlRequest, URLCompletion completion) {
+        super();
 
-		setURLRequest(urlRequest);
+        setURLRequest(urlRequest);
 
-		setURLCompletion(completion);
+        setURLCompletion(completion);
 
-		this.accumulationBuffer = new ByteArrayOutputStream();
-	}
+        this.accumulationBuffer = new ByteArrayOutputStream();
+    }
 
-	/**
-	 Returns the current {@link URLRequest} used.
+    /**
+     Returns the current {@link URLRequest} used.
 
-	 @return The {@link URLRequest} of this operation.
-	 */
-	public URLRequest getURLRequest() {
-		return this.urlRequest;
-	}
+     @return The {@link URLRequest} of this operation.
+     */
+    public URLRequest getURLRequest() {
+        return this.urlRequest;
+    }
 
-	/**
-	 Sets the current {@link URLRequest}. Cannot be null.
-	 */
-	public void setURLRequest(URLRequest urlRequest) {
-		if (urlRequest == null) {
-			throw new NullPointerException();
-		}
-		this.urlRequest = urlRequest;
-	}
+    /**
+     Sets the current {@link URLRequest}. Cannot be null.
+     */
+    public void setURLRequest(URLRequest urlRequest) {
+        if (urlRequest == null) {
+            throw new NullPointerException();
+        }
+        this.urlRequest = urlRequest;
+    }
 
-	/**
-	 Sets the {@link URLCompletion} interface that responds to this operation.
-	 */
-	protected void setURLCompletion(URLCompletion completion) {
-		this.completion = completion;
-	}
+    /**
+     Sets the {@link URLCompletion} interface that responds to this operation.
+     */
+    protected void setURLCompletion(URLCompletion completion) {
+        this.completion = completion;
+    }
 
-	/**
-	 Creates a new {@link OperationQueue} and adds this class which executes this operation.
-	 */
-	public void start() {
-		this.start(true);
-	}
+    /**
+     Creates a new {@link OperationQueue} and adds this class which executes this operation.
+     */
+    public void start() {
+        this.start(true);
+    }
 
-	/**
-	 Starts the operation without enqueuing it on an operation queue and running it on a thread.
-	 */
-	public void startSynchronous() {
-		this.start(false);
-	}
+    /**
+     Starts the operation without enqueuing it on an operation queue and running it on a thread.
+     */
+    public void startSynchronous() {
+        this.start(false);
+    }
 
-	/**
-	 Starts and runs this operation. If asynchronous is true an operation queue is created and the operation is
-	 put on its queue.
+    /**
+     Starts and runs this operation. If asynchronous is true an operation queue is created and the operation is
+     put on its queue.
 
-	 @param asynchronous A boolean value indicating if this operation should be started synchronously or asynchronously.
-	 */
-	public void start(boolean asynchronous) {
-		if (asynchronous) {
-			OperationQueue queue = new OperationQueue();
-			queue.addOperation(this);
-		} else {
-			this.execute();
-			this.complete();
-		}
-	}
+     @param asynchronous A boolean value indicating if this operation should be started synchronously or asynchronously.
+     */
+    public void start(boolean asynchronous) {
+        if (asynchronous) {
+            OperationQueue queue = new OperationQueue();
+            queue.addOperation(this);
+        } else {
+            this.execute();
+            this.complete();
+        }
+    }
 
-	/**
-	 The executing method of this operation.
+    /**
+     The executing method of this operation.
 
-	 This method runs in the worker thread of this operations operation queue.
-	 */
-	@Override
-	public synchronized void execute() {
-		super.execute();
+     This method runs in the worker thread of this operations operation queue.
+     */
+    @Override
+    public synchronized void execute() {
+        super.execute();
 
-		try {
+        try {
             // Write requestBody if any
             if (this.urlRequest.getHTTPBody() != null) {
                 this.urlRequest.setRequestProperty("Content-Length", "" + Integer.toString(this.urlRequest.getHTTPBody().length));
@@ -136,65 +136,65 @@ public class URLConnectionOperation extends BaseOperation {
 
                 this.urlRequest.getOutputStream().write(this.urlRequest.getHTTPBody());
             }
-            
-			InputStream is = urlRequest.getInputStream();
-			BufferedInputStream bin = new BufferedInputStream(is);
 
-			int c;
-			while (-1 != (c = bin.read())) {
-				this.accumulationBuffer.write(c);
-			}
-			bin.close();
-			is.close();
+            InputStream is = urlRequest.getInputStream();
+            BufferedInputStream bin = new BufferedInputStream(is);
 
-		} catch (IOException e) {
-			if (this.completion != null) {
-				this.completion.failure(this.urlRequest, e);
-			}
-		}
-	}
+            int c;
+            while (-1 != (c = bin.read())) {
+                this.accumulationBuffer.write(c);
+            }
+            bin.close();
+            is.close();
 
-	/**
-	 The complete method is called when this operation finishes executing.
-	 */
-	@Override
-	public synchronized void complete() {
-		super.complete();
+        } catch (IOException e) {
+            if (this.completion != null) {
+                this.completion.failure(this.urlRequest, e);
+            }
+        }
+    }
 
-		switch (getState()) {
-			case Rejected:
-				if (this.completion != null) {
-					this.completion.failure(this.urlRequest, new Throwable("URLRequestOperation rejected from operation queue"));
-				}
-				break;
-			case Cancelled:
-				if (this.completion != null) {
-					this.completion.failure(this.urlRequest, new Throwable("URLRequestOperation cancelled in operation queue"));
-				}
-				break;
-			default:
-				if (this.completion != null) {
-					this.completion.success(this.urlRequest, this.accumulationBuffer.toByteArray());
-				}
-				break;
-		}
+    /**
+     The complete method is called when this operation finishes executing.
+     */
+    @Override
+    public synchronized void complete() {
+        super.complete();
 
-		try {
-			this.accumulationBuffer.close();
-			this.accumulationBuffer = null;
-		} catch (IOException e) {}
+        switch (getState()) {
+            case Rejected:
+                if (this.completion != null) {
+                    this.completion.failure(this.urlRequest, new Throwable("URLRequestOperation rejected from operation queue"));
+                }
+                break;
+            case Cancelled:
+                if (this.completion != null) {
+                    this.completion.failure(this.urlRequest, new Throwable("URLRequestOperation cancelled in operation queue"));
+                }
+                break;
+            default:
+                if (this.completion != null) {
+                    this.completion.success(this.urlRequest, this.accumulationBuffer.toByteArray());
+                }
+                break;
+        }
 
-	}
+        try {
+            this.accumulationBuffer.close();
+            this.accumulationBuffer = null;
+        } catch (IOException e) {}
 
-	/**
-	 The failure method is called when the operation failed in working thread.
-	 */
-	@Override
-	public synchronized void failure(Throwable t) {
-		super.failure(t);
+    }
 
-		if (this.completion != null) {
-			this.completion.failure(this.urlRequest, t);
-		}
-	}
+    /**
+     The failure method is called when the operation failed in working thread.
+     */
+    @Override
+    public synchronized void failure(Throwable t) {
+        super.failure(t);
+
+        if (this.completion != null) {
+            this.completion.failure(this.urlRequest, t);
+        }
+    }
 }
