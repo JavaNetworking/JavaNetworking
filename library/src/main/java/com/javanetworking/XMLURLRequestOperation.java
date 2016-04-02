83875c9234202ce99268b4bf6a384ed09ce54e12
Gard Sandholt <gard.sandholt@gmail.com>
2016-04-02 13:24:00 +0200

    Fixed tab indentation

--- library/src/main/java/com/javanetworking/XMLURLRequestOperation.java
+++ library/src/main/java/com/javanetworking/XMLURLRequestOperation.java
@@ -19,78 +19,78 @@ import org.xml.sax.InputSource;
  - `text/xml`
  */
 public class XMLURLRequestOperation extends HTTPURLRequestOperation {
-	
-	/**
-	 A static constructor method that creates and returns a {@link XMLURLRequestOperation} instance.
-	 */
-	public static XMLURLRequestOperation operationWithURLRequest(URLRequest request, HTTPCompletion completion) {
-		return new XMLURLRequestOperation(request, completion);
-	}
 
-	/**
-	 Instantiate this class and sets the {@link URLRequest}, and the {@link HTTPCompletion} interface.
+    /**
+     A static constructor method that creates and returns a {@link XMLURLRequestOperation} instance.
+     */
+    public static XMLURLRequestOperation operationWithURLRequest(URLRequest request, HTTPCompletion completion) {
+        return new XMLURLRequestOperation(request, completion);
+    }
 
-	 This is the preferred constructor.
+    /**
+     Instantiate this class and sets the {@link URLRequest}, and the {@link HTTPCompletion} interface.
 
-	 @param urlConnection An open {@link URLRequest} to be used for HTTP network access.
-	 @param completion A {@link HTTPCompletion} instance that handles the completion interface methods.
-	 */
-	public XMLURLRequestOperation(URLRequest request, HTTPCompletion completion) {
-		super(request, null);
+     This is the preferred constructor.
 
-		this.setCompletion(completion);
-	}
-	
-	/**
-	 Get acceptable content types list for current connection. Default values for {@link XMLURLRequestOperation} is:
-	 
- 	 - `application/xml`
- 	 - `text/xml`
-	 
-	 @return A {@code List<String>} with acceptable content types.
-	 */
-	@Override
-	protected List<String> getAcceptableContentTypes() {
-		return HTTPURLRequestOperation.arrayToList(new String[] { "application/xml", "text/xml" });
-	}
-	
-	/**
-	 Sets the {@link HTTPCompletion} interface that responds to this operation.
-	 
-	 Parses the response data to {@link Document} object.
-	 */
-	@Override
-	protected void setCompletion(final HTTPCompletion completion) {
-		super.setCompletion(new HTTPCompletion() {
-			@Override
-			public void failure(URLRequest request, Throwable t) {
-				if (completion != null) {
-					completion.failure(request, t);
-				}
-			}
-			@Override
-			public void success(URLRequest request, Object responseData) {
-				if (completion != null) {
-					
-					String xmlContent = new String((byte[])responseData);
-					
-					Document document = null;
-					try {
-						DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-						DocumentBuilder builder = factory.newDocumentBuilder();
-						document = builder.parse(new InputSource(new StringReader(xmlContent)));
-						document.getDocumentElement().normalize();
+     @param urlConnection An open {@link URLRequest} to be used for HTTP network access.
+     @param completion A {@link HTTPCompletion} instance that handles the completion interface methods.
+     */
+    public XMLURLRequestOperation(URLRequest request, HTTPCompletion completion) {
+        super(request, null);
 
-						if (completion != null) {
-							completion.success(request, document);
-						}
-					} catch (Exception e) {
-						if (completion != null) {
-							completion.failure(request, e);
-						}
-					}
-				}
-			}
-		});
-	}
+        this.setCompletion(completion);
+    }
+
+    /**
+     Get acceptable content types list for current connection. Default values for {@link XMLURLRequestOperation} is:
+
+     - `application/xml`
+     - `text/xml`
+
+     @return A {@code List<String>} with acceptable content types.
+     */
+    @Override
+    protected List<String> getAcceptableContentTypes() {
+        return HTTPURLRequestOperation.arrayToList(new String[] { "application/xml", "text/xml" });
+    }
+
+    /**
+     Sets the {@link HTTPCompletion} interface that responds to this operation.
+
+     Parses the response data to {@link Document} object.
+     */
+    @Override
+    protected void setCompletion(final HTTPCompletion completion) {
+        super.setCompletion(new HTTPCompletion() {
+            @Override
+            public void failure(URLRequest request, Throwable t) {
+                if (completion != null) {
+                    completion.failure(request, t);
+                }
+            }
+            @Override
+            public void success(URLRequest request, Object responseData) {
+                if (completion != null) {
+
+                    String xmlContent = new String((byte[])responseData);
+
+                    Document document = null;
+                    try {
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = factory.newDocumentBuilder();
+                        document = builder.parse(new InputSource(new StringReader(xmlContent)));
+                        document.getDocumentElement().normalize();
+
+                        if (completion != null) {
+                            completion.success(request, document);
+                        }
+                    } catch (Exception e) {
+                        if (completion != null) {
+                            completion.failure(request, e);
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
